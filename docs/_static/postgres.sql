bb45c92a08f070d986455e29a1fae9d3883441ff
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-10-24 12:20:15 +0200

    inserts

--- docs/_static/postgres.sql
+++ docs/_static/postgres.sql
@@ -6,4 +6,13 @@ CREATE TABLE p_animal (
 
 ALTER TABLE p_animal ADD CONSTRAINT PK_p_animal PRIMARY KEY (sci_name);
 
-
+INSERT INTO p_animal VALUES ("Bufo", "Echte Kröte", "bufo");
+INSERT INTO p_animal VALUES ("Vulpes vulpes", "Rotfuchs", "red fox");
+INSERT INTO p_animal VALUES ("Giraffa camelopardalis", "Giraffe", "giraffe");
+INSERT INTO p_animal VALUES ("Elephas maximus", "Asiatischer Elefant", "asian elephant");
+INSERT INTO p_animal VALUES ("Rattus norvegicus", "Wanderratte", "brown rat");
+INSERT INTO p_animal VALUES ("Sciurus vulgaris", "Eichhörnchen", "red squirrel");
+INSERT INTO p_animal VALUES ("Microchiroptera", "Fledermaus", "microbat");
+INSERT INTO p_animal VALUES ("Felis silvestris f. catus", "Hauskatze", "cat");
+INSERT INTO p_animal VALUES ("Anguis fragilis", "Blindschleiche", "slow worm");
+INSERT INTO p_animal VALUES ("Morelia viridis", "Grüner Baumpython", "green tree python");
