20e95492b16ccf0e2ccadf6601a9d8938db50280
Andreas Willinger <awi95@gmx.at>
2014-11-10 21:52:16 +0100

    Added trigger, log tables

--- docs/_static/mysql.sql
+++ docs/_static/mysql.sql
@@ -20,3 +20,37 @@ ALTER TABLE translation ADD CONSTRAINT PK_translation PRIMARY KEY (wname,languag
 
 ALTER TABLE translation ADD CONSTRAINT FK_translation_0 FOREIGN KEY (wname) REFERENCES m_animal (wname);
 ALTER TABLE translation ADD CONSTRAINT FK_translation_1 FOREIGN KEY (language) REFERENCES language (language);
+
+-- TRIGGER
+CREATE TABLE translationlog
+(
+    wnameold VARCHAR(255),
+    languageold VARCHAR(64),
+    tnameold VARCHAR(255),
+    wnamenew VARCHAR(255),
+    languagenew VARCHAR(64),
+    tnamenew VARCHAR(255),
+    action ENUM('new', 'update', 'delete'),
+    lstamp INTEGER
+)ENGINE=INNODB;
+
+DELIMITER //
+CREATE TRIGGER after_insert_translation AFTER INSERT ON translation
+FOR EACH ROW
+BEGIN
+    INSERT INTO translationlog VALUES(NULL, NULL, NULL, NEW.wname, NEW.language, NEW.tname, 'new', UNIX_TIMESTAMP(NOW()));
+END;//
+
+CREATE TRIGGER after_update_translation AFTER UPDATE ON translation
+FOR EACH ROW
+BEGIN
+    INSERT INTO translationlog VALUES(OLD.wname, OLD.language, OLD.tname, NEW.wname, NEW.language, NEW.tname, 'update', UNIX_TIMESTAMP(NOW()));
+END;//
+
+CREATE TRIGGER after_delete_translation AFTER DELETE ON translation
+FOR EACH ROW
+BEGIN
+    INSERT INTO translationlog VALUES(OLD.wname, OLD.language, OLD.tname, NULL, NULL, NULL, 'delete', UNIX_TIMESTAMP(NOW()));
+END;//
+
+DELIMITER ;
--- docs/_static/postgres.sql
+++ docs/_static/postgres.sql
@@ -6,13 +6,59 @@ CREATE TABLE p_animal (
 
 ALTER TABLE p_animal ADD CONSTRAINT PK_p_animal PRIMARY KEY (wname);
 
-INSERT INTO p_animal VALUES ("Bufo", "Echte Kröte", "bufo");
-INSERT INTO p_animal VALUES ("Vulpes vulpes", "Rotfuchs", "red fox");
-INSERT INTO p_animal VALUES ("Giraffa camelopardalis", "Giraffe", "giraffe");
-INSERT INTO p_animal VALUES ("Elephas maximus", "Asiatischer Elefant", "asian elephant");
-INSERT INTO p_animal VALUES ("Rattus norvegicus", "Wanderratte", "brown rat");
-INSERT INTO p_animal VALUES ("Sciurus vulgaris", "Eichhörnchen", "red squirrel");
-INSERT INTO p_animal VALUES ("Microchiroptera", "Fledermaus", "microbat");
-INSERT INTO p_animal VALUES ("Felis silvestris f. catus", "Hauskatze", "cat");
-INSERT INTO p_animal VALUES ("Anguis fragilis", "Blindschleiche", "slow worm");
-INSERT INTO p_animal VALUES ("Morelia viridis", "Grüner Baumpython", "green tree python");
+INSERT INTO p_animal VALUES ('Bufo', 'Echte Kröte', 'bufo');
+INSERT INTO p_animal VALUES ('Vulpes vulpes', 'Rotfuchs', 'red fox');
+INSERT INTO p_animal VALUES ('Giraffa camelopardalis', 'Giraffe', 'giraffe');
+INSERT INTO p_animal VALUES ('Elephas maximus', 'Asiatischer Elefant', 'asian elephant');
+INSERT INTO p_animal VALUES ('Rattus norvegicus', 'Wanderratte', 'brown rat');
+INSERT INTO p_animal VALUES ('Sciurus vulgaris', 'Eichhörnchen', 'red squirrel');
+INSERT INTO p_animal VALUES ('Microchiroptera', 'Fledermaus', 'microbat');
+INSERT INTO p_animal VALUES ('Felis silvestris f. catus', 'Hauskatze', 'cat');
+INSERT INTO p_animal VALUES ('Anguis fragilis', 'Blindschleiche', 'slow worm');
+INSERT INTO p_animal VALUES ('Morelia viridis', 'Grüner Baumpython', 'green tree python');
+
+-- TRIGGER
+CREATE TABLE animallog
+(
+    wnameold VARCHAR(255),
+    ger_nameold VARCHAR(255),
+    eng_nameold VARCHAR(255),
+    wnamenew VARCHAR(255),
+    ger_namenew VARCHAR(255),
+    eng_namenew VARCHAR(255),
+    action VARCHAR(16),
+    lstamp INTEGER
+);
+
+CREATE OR REPLACE FUNCTION handle_delete() RETURNS trigger AS $handle_delete$
+BEGIN
+    INSERT INTO animallog VALUES(OLD.wname, OLD.ger_name, OLD.eng_name, NULL, NULL, NULL, 'delete', extract(epoch from now()));
+    
+    RETURN OLD;
+END;
+$handle_delete$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION handle_update() RETURNS trigger AS $handle_update$
+BEGIN
+    INSERT INTO animallog VALUES(OLD.wname, OLD.ger_name, OLD.eng_name, NEW.wname, NEW.ger_name, NEW.eng_name, 'update', extract(epoch from now()));
+    
+    RETURN NEW;
+END;
+$handle_update$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION handle_new() RETURNS trigger AS $handle_new$
+BEGIN
+    INSERT INTO animallog VALUES(NULL, NULL, NULL, NEW.wname, NEW.ger_name, NEW.eng_name, 'new', extract(epoch from now()));
+    
+    RETURN NEW;
+END;
+$handle_new$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_insert_animal AFTER INSERT ON p_animal
+FOR EACH ROW EXECUTE PROCEDURE handle_new();
+
+CREATE TRIGGER after_update_animal AFTER UPDATE ON p_animal
+FOR EACH ROW EXECUTE PROCEDURE handle_update();
+
+CREATE TRIGGER after_delete_animal AFTER DELETE ON p_animal
+FOR EACH ROW EXECUTE PROCEDURE handle_delete();
