83abb4d6f277aa86a516646e5ba980f8520b9920
Andreas Willinger <awi95@gmx.at>
2014-11-12 10:52:11 +0100

    Added very ugly implementation

---
+++ src/dbsync/Animallog.java
@@ -0,0 +1,11 @@
+public class Animallog
+{
+	public String wnameold;
+	public String ger_nameold;
+	public String eng_nameold;
+	public String wnamenew;
+	public String ger_namenew;
+	public String eng_namenew;
+	public String action;
+	public Integer lstamp;
+}
